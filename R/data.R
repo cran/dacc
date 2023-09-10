63273452fd3a833f5cf86cb69b6580841d2b4cdd
Yan Li <yan.4.li@uconn.edu>
2023-09-10 08:00:02 +0000

    version 0.0-1

---
+++ R/data.R
@@ -0,0 +1,21 @@
+##' Sample Data Used in Simulation Studies of 
+##' "Regularized Fingerprinting in Detection and Attribution of Climate Change 
+##' with Weight Matrix Optimizing the Efficiency in Scaling Factor Estimation".
+##'
+##' A data list of designed covariance matrix and the expected responses 
+##' to the two forcings ANT and NAT with name
+##' \code{Cov} and \code{X}
+##' where
+##' \itemize{
+##'     \item \code{Cov}: a list of the true covariance matrices;
+##'     \item \code{X}: a data matrix of the expected responses to external forcing;
+##' }
+##'
+##' @docType data
+##' @name simDat
+##' @usage data(simDat)
+##' @format A data list with two separate data sets.
+##' @keywords datasets
+##' @examples
+##' data(simDat)
+"simDat"
